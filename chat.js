8ddaf4f16c23d511498a585f3222527383ea768f
NeerajKumar201 <63168156+NeerajKumar201@users.noreply.github.com>
2023-02-16 22:28:10 +0530

    Files Upload
    First time uploaded all files

---
+++ chat.js
@@ -0,0 +1,9 @@
+function fun(){
+
+    if (document.getElementById("main").style.display== "block"){
+        document.getElementById("main").style.display= "none";
+    }else{
+        document.getElementById("main").style.display= "block";
+    }
+   
+}
