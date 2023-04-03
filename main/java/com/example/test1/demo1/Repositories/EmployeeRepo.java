8cf16e0c30ef1dd2e1dca8473de538fadf875cec
NeerajKumar201 <63168156+NeerajKumar201@users.noreply.github.com>
2023-04-04 01:50:11 +0530

    Delete main directory

--- main/java/com/example/test1/demo1/Repositories/EmployeeRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.test1.demo1.Repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.test1.demo1.Entities.Employee;
-
-public interface EmployeeRepo extends JpaRepository<Employee,Integer>{
-    
-}
